cb9f8e46207d749e68b93bb7cacca60389db8d37
pentiumx <pentiumx@live.jp>
2013-12-29 11:55:43 +0900

    refs #1632 Enable seeding the database
    with seeds.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,38 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+include ActionDispatch::TestProcess
+require "#{Rails.root}/app/controllers/model_data_service"
+
+# ModelData
+params = {
+  model_datum: {
+    file: fixture_file_upload('spec/fixtures/files/notexture.fbx'),
+    title: 'seed data'
+  }
+}
+converter = ModelDataService.new()
+model_datum = converter.convert_model_datum(params)
+model_datum.save!
+
+
+# Stage
+valid_file = File.new(Rails.root.join('spec', 'fixtures', 'files', 'backgrounddetailed6.jpg'))
+params = {
+  stage: {
+    file: fixture_file_upload('spec/fixtures/files/terrain0.json'),
+    texture: {
+      data: fixture_file_upload('spec/fixtures/files/backgrounddetailed6.jpg', 'image/png')
+    }
+  }
+}
+
+file = params[:stage][:file]
+jsonstring = file.read
+stage = Stage.new(scene_data: jsonstring, title: params[:stage][:title])
+unless params[:stage][:texture].nil?
+  textures = Texture.new(data: params[:stage][:texture][:data])#'data'
+  stage.textures << textures
+end
+stage.save!
